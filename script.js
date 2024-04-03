9bf0a93b1f79f2393e1f2e37901ce49740ef3a26
YacineDah <adayacineda@gmail.com>
2024-04-03 18:03:18 +0000

    New way to extract price using regular expression

--- script.js
+++ script.js
@@ -21,16 +21,53 @@ var months = {
     "November":11,
     "December":12
 };
+function clean_price(num_data,min,max) {
+    num_data = num_data.replace(",",".").replace(/(\d+)([a-zA-Z]+)/g, '$1 $2');
+    var len_min = Math.abs(min).toString().length
+    var len_max = Math.abs(max).toString().length
+    var regex1 = new RegExp(`\\b\\d{${len_min}}\\b`, 'g');
+    var regex2 = new RegExp(`\\b\\d{${len_min === 3 ? 1 : len_min-3}}\\.\\d{3}\\b`, 'g');
+    var regex3 = new RegExp(`\\b\\d{${len_max}}\\b`, 'g');
+    var regex4 = new RegExp(`\\b\\d{${len_max-3}}\\.\\d{3}\\b`, 'g');
+    var price = num_data.match(regex2) !== null ? num_data.match(regex2) : num_data.match(regex1) !== null ? num_data.match(regex1) : null;
+    console.log(price);
+    if (price === null) {
+        price = num_data.match(regex4) !== null ? num_data.match(regex4) : num_data.match(regex3) !== null ? num_data.match(regex3) : null;
+        console.log(price);
+        if (price !== null) {
+            console.log(price);
+            price = price.map(number => number.replace(/[,.]/g, ''));
+            console.log(price);
+            var final_price = parseInt(price.filter(number => {
+                var num = parseInt(number);
+                if (num >= min && num <= max) {
+                    return true; // Include the number in the filtered array
+                } else {
+                    return false; // Exclude the number from the filtered array
+                }
+            })[0]);
+        } else {
+            return NaN;
+        }
+    } else {
+        console.log(price);
+        price = price.map(number => number.replace(/[,.]/g, ''));
+        console.log(price);
+        var final_price = parseInt(price.filter(number => {
+            var num = parseInt(number);
+            if (num >= min && num <= max) {
+                return true; // Include the number in the filtered array
+            } else {
+                return false; // Exclude the number from the filtered array
+            }
+        })[0]);
+    }
+    return final_price;
+}
 function scraper(name, min, max) {
-    scrolling = setInterval(function () {
-        scrollBy(0, document.body.scrollHeight);
-        console.log("scrolling ...");
-        if (document.getElementsByClassName("img _55ym _55yq _55yo").length === 1) {
-            scrollBy(0, document.body.scrollHeight);
-            clearInterval(scrolling);
-            console.log("stopped .");
-            var fg = document.getElementsByClassName("_5rgt _5nk5 _5wnf _5msi");
+    var fg = document.getElementsByClassName("_5rgt _5nk5 _5wnf _5msi");
     var sg = document.getElementsByClassName("_5rgt _5nk5 _5msi");
+    var tg = document.querySelectorAll("#u_d_c_Y1");
     var des = [];
     var deals_local = {
         "prices": [],
@@ -44,33 +81,45 @@ function scraper(name, min, max) {
     for (i=0; i<sg.length; i++) {
         des.push(sg[i]);
     }
+    for (i=0; i<tg.length; i++) {
+        des.push(tg[i]);
+    }
     des.forEach(function (value, index, array) {
-        clean = value.textContent.replaceAll(".","").replaceAll(",","").toLocaleLowerCase();
+        clean = value.textContent.toLocaleLowerCase();
         isMultiple = value.textContent.split("00").length > 3 ? true : false;
         batt = value.textContent.toLocaleLowerCase().includes("mah");
         phone_exist = value.textContent.toLocaleLowerCase().includes(name);
         console.log(value.textContent);
-        num_data = clean.replaceAll(" ","").split(name)[1];
-        price = isMultiple && phone_exist && num_data.match(/\d+/g) !== null ? num_data.match(/\d+/g).filter(e => e.includes("00") && e.length >= Math.abs(min).toString().length && e.length <= Math.abs(max).toString().length+2 && parseInt(e.slice(0,5)) >= min && parseInt(e.slice(0,5)) <= max )[0] : clean.match(/\d+/g) !== null ? clean.match(/\d+/g).filter(e => e.includes("00") && e.length >= Math.abs(min).toString().length && e.length <= Math.abs(max).toString().length+2 && parseInt(e.slice(0,5)) >= min && parseInt(e.slice(0,5)) <= max )[0] : null;
-        if (isMultiple && phone_exist && price !== undefined) {
-            deals_local.prices.push(price.slice(0,5));
-            deals_local.links.push(value.querySelectorAll("a")[value.querySelectorAll("a").length - 1]);
-            deals_local.date.push(value.previousElementSibling.querySelector("abbr").textContent);
-            deals_local.des.push(value.textContent);
-            console.log(price.includes("0000") ? price.slice(0,5) : price);
-        } else if (clean.match(/\d+/g) !== null && price !== undefined) {
-            var price_2 = clean.match(/\d+/g).filter(e => e.includes("00") && e.length >= Math.abs(min).toString().length && e.length <= Math.abs(max).toString().length+2 && parseInt(e.slice(0,5)) >= min && parseInt(e.slice(0,5)) <= max )[0];
-            deals_local.prices.push(price_2.slice(0,5));
-            deals_local.links.push(value.querySelectorAll("a")[value.querySelectorAll("a").length - 1]);
-            deals_local.date.push(value.previousElementSibling.querySelector("abbr").textContent);
-            deals_local.des.push(value.textContent);
-            console.log(price_2.includes("0000") ? price_2.slice(0,5) : price_2);
+        num_data = clean.split(name)[1];
+        console.log(num_data);
+        if (clean === "") {
+            return ;
+        } else if (isMultiple && phone_exist && num_data !== undefined) {
+            console.log(clean_price(clean,min,max));
+            if ( !isNaN(clean_price(num_data,min,max)) ) {
+                price = clean_price(num_data,min,max);
+                deals_local.prices.push(price);
+                deals_local.links.push(value.querySelectorAll("a")[value.querySelectorAll("a").length - 1]);
+                deals_local.date.push(value.previousElementSibling.querySelector("abbr").textContent);
+                deals_local.des.push(value.textContent);
+                console.log(price);  
+            }   
+        } else if (clean.match(/\d+/g) !== null) {
+            console.log(clean_price(clean,min,max));
+            if ( !isNaN(clean_price(clean,min,max)) ) {
+                price = clean_price(clean,min,max);
+                deals_local.prices.push(price);
+                deals_local.links.push(value.querySelectorAll("a")[value.querySelectorAll("a").length - 1]);
+                deals_local.date.push(value.previousElementSibling.querySelector("abbr").textContent);
+                deals_local.des.push(value.textContent);
+                console.log(price); 
+            }
         }
     });
     deals = deals_local;
     ndes = des;
     for (i=0; i<deals.prices.length; i++) {
-        if (deals.date[i].includes("2022") || months[deals.date[i].split(" ")[0]] < 7) {
+        if (deals.date[i].includes("2023") || deals.date[i].includes("2022") || months[deals.date[i].split(" ")[0]] > 3) {
             deals.prices[i] = 0;
         } else {
             x.push(deals.prices[i] + " " + deals.links[i] + "\n" + deals.date[i] + "\n" + deals.des[i]);
@@ -85,10 +134,7 @@ function scraper(name, min, max) {
     const maxi = Math.max(...deals.prices.map(e => parseFloat(e)));
     const mini = Math.min(...deals.prices.filter(e => e !== 0).map(e => parseFloat(e)));
     const sum = deals.prices.map(e => parseFloat(e)).reduce((accumulator, value) => accumulator + value, 0);
-    console.log(`The minimum price is at ${mini} The maximum is ${maxi} and the average is ${sum/(deals.prices.filter(e => e !== 0).length)}`);
-        }
-    }, 100);
-    
+    console.log(`The minimum price is at ${mini} The maximum is ${maxi} and the average is ${sum/(deals.prices.filter(e => e !== 0).length)}`);   
 }
 function search(name, stat) {
     var sp = name.split(" ");
@@ -101,7 +147,7 @@ function search(name, stat) {
             }
         }
     if (stat === "new") {
-        window.location.href = "https://m.facebook.com/search/posts?q="+ b +"&filters=eyJyZWNlbnRfcG9zdHM6MCI6IntcIm5hbWVcIjpcInJlY2VudF9wb3N0c1wiLFwiYXJnc1wiOlwiXCJ9In0%3D";
+        window.location.href = "https://m.facebook.com/search/posts?q="+ b +"%20دج&filters=eyJyZWNlbnRfcG9zdHM6MCI6IntcIm5hbWVcIjpcInJlY2VudF9wb3N0c1wiLFwiYXJnc1wiOlwiXCJ9In0%3D";
         window.open("https://m.facebook.com/search/posts?q="+ b +"%20da&filters=eyJyZWNlbnRfcG9zdHM6MCI6IntcIm5hbWVcIjpcInJlY2VudF9wb3N0c1wiLFwiYXJnc1wiOlwiXCJ9In0%3D",'_blank');
         window.open("https://m.facebook.com/search/posts?q="+ b +"%2058&filters=eyJyZWNlbnRfcG9zdHM6MCI6IntcIm5hbWVcIjpcInJlY2VudF9wb3N0c1wiLFwiYXJnc1wiOlwiXCJ9In0%3D",'_blank');
     } else if (stat === "old") {
